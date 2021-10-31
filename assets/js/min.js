8ef4663869786cd0ff24eaa40e35e1f46e179dc0
Jubaear <jubaearislam86664@gmail.com>
2021-10-31 21:20:01 +0600

    new page add

--- assets/js/min.js
+++ assets/js/min.js
@@ -22,4 +22,76 @@
         animate: 2000,
     });
 
+
+    var options = {
+        animationEnabled: true,
+        title: {
+            // text: "Daily Progress"
+        },
+        axisX: {
+            valueFormatString: "MMM"
+        },
+        axisY: {
+            // title: "Sales (in USD)",
+            prefix: "$",
+        },
+        data: [{
+            yValueFormatString: "$#,###",
+            xValueFormatString: "MMMM",
+            yValueFormatString: "#0.0" % "",
+            type: "spline",
+            dataPoints: [{
+                    x: new Date(2021, 0),
+                    y: 25060
+                },
+                {
+                    x: new Date(2021, 1),
+                    y: 27980
+                },
+                {
+                    x: new Date(2021, 2),
+                    y: 33800
+                },
+                {
+                    x: new Date(2021, 3),
+                    y: 49400
+                },
+                {
+                    x: new Date(2021, 4),
+                    y: 40260
+                },
+                {
+                    x: new Date(2021, 5),
+                    y: 33900
+                },
+                {
+                    x: new Date(2021, 6),
+                    y: 48000
+                },
+                {
+                    x: new Date(2021, 7),
+                    y: 31500
+                },
+                {
+                    x: new Date(2021, 8),
+                    y: 32300
+                },
+                {
+                    x: new Date(2021, 9),
+                    y: 42000
+                },
+                {
+                    x: new Date(2021, 10),
+                    y: 52160
+                },
+                {
+                    x: new Date(2021, 11),
+                    y: 49400
+                }
+            ]
+        }]
+    };
+    $("#chartContainer").CanvasJSChart(options);
+
+
 })(jQuery);
