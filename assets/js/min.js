1b0a89f091130e46c07fbdc1734920aead4e36fc
Jubaear <jubaearislam86664@gmail.com>
2021-11-01 20:05:59 +0600

    responsive

--- assets/js/min.js
+++ assets/js/min.js
@@ -1,26 +1,26 @@
-(function ($) {
+(function($) {
     'use strict'
 
     $('.columns').theiaStickySidebar();
 
-    $('.Unit').easyPieChart({
-        size: 110,
-        barColor: "#E56035",
-        scaleLength: 0,
-        lineWidth: 13,
-        trackColor: "#FCDDCF",
-        lineCap: "circle",
-        animate: 2000,
-    });
-    $('.hours2').easyPieChart({
-        size: 110,
-        barColor: "#499A9A",
-        scaleLength: 0,
-        lineWidth: 13,
-        trackColor: "#E8F1F1",
-        lineCap: "circle",
-        animate: 2000,
-    });
+    // $('.Unit').easyPieChart({
+    //     size: 110,
+    //     barColor: "#E56035",
+    //     scaleLength: 0,
+    //     lineWidth: 13,
+    //     trackColor: "#FCDDCF",
+    //     lineCap: "circle",
+    //     animate: 2000,
+    // });
+    // $('.hours2').easyPieChart({
+    //     size: 110,
+    //     barColor: "#499A9A",
+    //     scaleLength: 0,
+    //     lineWidth: 13,
+    //     trackColor: "#E8F1F1",
+    //     lineCap: "circle",
+    //     animate: 2000,
+    // });
 
 
     var options = {
